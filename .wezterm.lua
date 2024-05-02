bc9012bf05d493a9b272dd4d0a27a20ce49104ca
Danilo Marques <danilomarques20@hotmail.com>
2024-05-01 23:10:47 -0300

    Updating wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,114 +1,59 @@
-local wezterm = require("wezterm")
-local config = {}
-config.font = wezterm.font("Bitstream Vera Sans Mono")
+local wezterm = require 'wezterm'
 
-config.use_fancy_tab_bar = false
-local mux = wezterm.mux
+local config = wezterm.config_builder()
 
-wezterm.on("gui-startup", function(cmd)
-	local _, _, window = mux.spawn_window(cmd or {})
-	window:gui_window():maximize()
-end)
+config.font = wezterm.font 'Hack Nerd Font Mono'
+config.color_scheme = 'Bim (Gogh)'
+config.font_size = 12
+config.adjust_window_size_when_changing_font_size = false
+config.tab_bar_at_bottom = true
+config.front_end = "Software"
 
-local act = wezterm.action
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+config.colors = {
+	background = '#1e1e2e'
+}
 
 config.leader = { key = "q", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
-	--- splitting
 	{
-		mods = "LEADER",
-		key = "-",
-		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+		mods = "LEADER|SHIFT",
+		key = "%",
+		action = wezterm.action.SplitHorizontal({domain="CurrentPaneDomain"}),
 	},
 	{
-		mods = "LEADER",
-		key = "=",
-		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+		mods = "LEADER|SHIFT",
+		key = '"',
+		action = wezterm.action.SplitVertical({domain="CurrentPaneDomain"}),
 	},
-
 	{
-		mods = "LEADER",
-		key = "m",
-		action = wezterm.action.TogglePaneZoomState,
+		key="c",
+		mods="LEADER",
+		action=wezterm.action{SpawnTab="CurrentPaneDomain"}
 	},
-	{ key = "n", mods = "LEADER", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
-	-- other keys
-}
-config.colors = {
-	-- The default text color
-	foreground = "silver",
-	-- The default background color
-	background = "black",
+	-- navigate tabs keymaps
+	{key="h", mods="CTRL", action=wezterm.action{ActivateTabRelative=-1}},
+	{key="j", mods="CTRL", action=wezterm.action{ActivateTabRelative=1}},
+	{key="1", mods="LEADER", action=wezterm.action{ActivateTab=0}},
+	{key="2", mods="LEADER", action=wezterm.action{ActivateTab=1}},
+	{key="3", mods="LEADER", action=wezterm.action{ActivateTab=2}},
+	{key="4", mods="LEADER", action=wezterm.action{ActivateTab=3}},
+	{key="5", mods="LEADER", action=wezterm.action{ActivateTab=4}},
+	{key="6", mods="LEADER", action=wezterm.action{ActivateTab=5}},
+	{key="7", mods="LEADER", action=wezterm.action{ActivateTab=6}},
+	{key="8", mods="LEADER", action=wezterm.action{ActivateTab=7}},
+	{key="9", mods="LEADER", action=wezterm.action{ActivateTab=8}},
+	{key="j", mods="LEADER", action=wezterm.action{ActivatePaneDirection="Down"}},
+	{key="k", mods="LEADER", action=wezterm.action{ActivatePaneDirection="Up"}},
+	{key="h", mods="LEADER", action=wezterm.action{ActivatePaneDirection="Left"}},
+	{key="l", mods="LEADER", action=wezterm.action{ActivatePaneDirection="Right"}},
+	{key="x", mods="LEADER", action=wezterm.action{CloseCurrentTab={confirm=true}}},
 
-	-- Overrides the cell background color when the current cell is occupied by the
-	-- cursor and the cursor style is set to Block
-	cursor_bg = "#52ad70",
-	-- Overrides the text color when the current cell is occupied by the cursor
-	cursor_fg = "black",
-	-- Specifies the border color of the cursor when the cursor style is set to Block,
-	-- or the color of the vertical or horizontal bar when the cursor style is set to
-	-- Bar or Underline.
-	cursor_border = "#52ad70",
-
-	-- the foreground color of selected text
-	selection_fg = "black",
-	-- the background color of selected text
-	selection_bg = "#fffacd",
-
-	-- The color of the scrollbar "thumb"; the portion that represents the current viewport
-	scrollbar_thumb = "#222222",
-
-	-- The color of the split lines between panes
-	split = "#444444",
-
-	ansi = {
-		"black",
-		"maroon",
-		"green",
-		"olive",
-		"navy",
-		"purple",
-		"teal",
-		"silver",
-	},
-	brights = {
-		"grey",
-		"red",
-		"lime",
-		"yellow",
-		"blue",
-		"fuchsia",
-		"aqua",
-		"white",
-	},
-
-	-- Arbitrary colors of the palette in the range from 16 to 255
-	indexed = { [136] = "#af8700" },
-
-	-- Since: 20220319-142410-0fcdea07
-	-- When the IME, a dead key or a leader key are being processed and are effectively
-	-- holding input pending the result of input composition, change the cursor
-	-- to this color to give a visual cue about the compose state.
-	compose_cursor = "orange",
-
-	-- Colors for copy_mode and quick_select
-	-- available since: 20220807-113146-c2fee766
-	-- In copy_mode, the color of the active text is:
-	-- 1. copy_mode_active_highlight_* if additional text was selected using the mouse
-	-- 2. selection_* otherwise
-	copy_mode_active_highlight_bg = { Color = "#000000" },
-	-- use `AnsiColor` to specify one of the ansi color palette values
-	-- (index 0-15) using one of the names "Black", "Maroon", "Green",
-	--  "Olive", "Navy", "Purple", "Teal", "Silver", "Grey", "Red", "Lime",
-	-- "Yellow", "Blue", "Fuchsia", "Aqua" or "White".
-	copy_mode_active_highlight_fg = { AnsiColor = "Black" },
-	copy_mode_inactive_highlight_bg = { Color = "#52ad70" },
-	copy_mode_inactive_highlight_fg = { AnsiColor = "White" },
-
-	quick_select_label_bg = { Color = "peru" },
-	quick_select_label_fg = { Color = "#ffffff" },
-	quick_select_match_bg = { AnsiColor = "Navy" },
-	quick_select_match_fg = { Color = "#ffffff" },
 }
 
 return config
